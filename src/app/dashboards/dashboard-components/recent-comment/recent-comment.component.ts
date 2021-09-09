71dfa2decfaa70fb148621ad28bbb71748015894
Oscar Avila <oscaravilaa9817@gmail.com>
2021-09-08 21:37:53 -0500

    registro de sensores con firebase

--- src/app/dashboards/dashboard-components/recent-comment/recent-comment.component.ts
+++ src/app/dashboards/dashboard-components/recent-comment/recent-comment.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import { FireDataService } from 'src/app/appServices/dashboards/services/fire-data.service';
+import { Carro } from 'src/app/interfaces/carro';
+
 
 @Component({
   selector: 'app-recent-comment',
@@ -8,172 +10,44 @@ import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 })
 export class RecentcommentComponent {
 
+  recentcomments: Object[] = [];
+
   public config: PerfectScrollbarConfigInterface = {};
   
-  constructor() {}
+  constructor(private fireData: FireDataService) {}
 
-  recentcomments: Object[] = [
-    {
-      image: 'assets/images/users/1.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/2.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/3.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/4.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/3.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/2.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/3.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/4.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/1.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/1.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/1.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/1.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/1.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/1.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/1.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/1.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/1.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-    {
-      image: 'assets/images/users/1.jpg',
-      name: 'James Anderson',
-      comment:
-        'Ingresado con éxito',
-      date: 'April 14, 2016 | 14h00',
-      status: 'Approved',
-      labelcolor: 'badge-light-success text-success'
-    },
-  ];
+  ngOnInit(): void {
+    this.leerData();
+  }
+
+  leerData() {
+    this.fireData.getCarros().subscribe(info => {
+      var fecha: string;
+      var hora: string;
+      const lista = [];
+      for(var i = 0; i < info.length; i++){
+        if(info[i].stream.split('|')[2] === '1'){
+          lista.push({
+            image: 'assets/images/users/1.jpg',
+            name: 'Pepito Pérez Gonzalez',
+            comment: 'Ha abierto la puerta del auto',
+            date: info[i].stream.split('|')[0]+" | "+info[i].stream.split('|')[1],
+            status: 'Approved',
+            labelColor: 'badge-light-success text-success'
+          })
+        }
+        if (info[i].stream.split('|')[3] === '1') {
+          lista.push({
+            image: 'assets/images/users/4.jpg',
+            name: 'Susana Romero',
+            comment: 'Se ha sentado en el asiento',
+            date: info[i].stream.split('|')[0]+" | "+info[i].stream.split('|')[1],
+            status: 'Approved',
+            labelColor: 'badge-light-success text-success'
+          })
+        }
+      }
+      this.recentcomments = lista;
+    });
+  }
 }
